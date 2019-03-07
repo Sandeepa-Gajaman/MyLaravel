24972377c710c56034948dc0e87e452583519f7e
Sandeepa Gajaman <sandeepagajaman@gmailcom>
2019-03-07 20:19:20 +0530

    Creating a form to upload images

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>My Laravel</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.3/css/foundation.css">
+</head>
+
+<body>
+<div class="row">
+    @yield('content')
+</div>
+</body>
+</html>
